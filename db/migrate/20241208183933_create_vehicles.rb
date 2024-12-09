f8321a390668f40f5924f0fe630f932535af02a9
Hanhiz <haanh24094@gmail.com>
2024-12-09 18:23:29 +0700

    Polishing plate boiler content

--- db/migrate/20241208183933_create_vehicles.rb
+++ db/migrate/20241208183933_create_vehicles.rb
@@ -1,12 +1,14 @@
 class CreateVehicles < ActiveRecord::Migration[7.0]
   def change
-    create_table :vehicles do |t|
-      t.integer :vehicle_type
-      t.integer :capacity
-      t.string :model
-      t.string :registration_number
+    unless table_exists?(:vehicles)
+      create_table :vehicles do |t|
+        t.integer :vehicle_type
+        t.integer :capacity
+        t.string :model
+        t.string :registration_number
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
